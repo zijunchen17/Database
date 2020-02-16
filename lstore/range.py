e8897b6e1b3a9b737b9b2b314f4b23f43959b3ba
Vsevolod Moskaev <vsmoskaev@ucdavis.edu>
2020-02-15 20:18:06 -0800

    add more functions to Page_Range class

--- lstore/range.py
+++ lstore/range.py
@@ -4,19 +4,36 @@ import math
 
 class Page_Range:
 
-    def __init__(self):
+    def __init__(self, all_columns):
 
-        self.base_pages = [[Page()] for _ in range(self.all_columns)] * BASE_PAGES_PER_RANGE
-        self.tail_pages = [[Page()] for _ in range(self.all_columns)]
-        
-        def getBasePageIndex(self, rid):
+        self.full = 0
+        self.base_pages = [[Page()] for _ in range(all_columns)]
+        self.tail_pages = [[Page()] for _ in range(all_columns)]
 
-            return rid // (PAGE_SIZE // RECORD_SIZE)
-        
-        def getBasePage(self, index):
+    def getBasePageIndex(self, rid):
 
-            return self.base_pages[index]
-        
+        return rid // (PAGE_SIZE // RECORD_SIZE)
+    
+    def getBasePage(self, index):
+
+        return self.base_pages[index]
+    
+    def getLastBasePage(self):
+
+        return [column[-1] for column in self.base_pages]
+
+    def __addBasePage(self):
+
+        if not self.full:
+            if not self.base_pages[0][-1].has_capacity():
+                for page_list in self.base_pages:
+                    page_list.append(Page())
+
+    def __addTailPage(self):
+
+        if not self.tail_pages[0][-1].has_capacity():
+            for page_list in self.tail_pages:
+                page_list.append(Page())
         
 
 
