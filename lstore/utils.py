444f1568c2f950075b142bfebda4b749a21b8984
greg graham <greggraham256@gmail.com>
2020-02-28 02:58:00 -0800

    Shortened utils.parse_page_attributes_from_filename()

--- lstore/utils.py
+++ lstore/utils.py
@@ -4,21 +4,13 @@ def get_page_range_index(rid):
     return (rid - 1) // (PAGE_SIZE // RECORD_SIZE * BASE_PAGES_PER_RANGE)
 
 
-def parse_page_attributes_from_filename(string):
+def parse_page_attributes_from_filename(path):
     # Example input: "ECS165/Grades/page_range0/base/column1"
 
-    def find_last(target,input):
-        for index in reversed(range(len(input))):
-            if input[index] == target:
-                return index
-
-    # table_name = range_num = page_type = col_num = ''
-    tokenized = string.split('/')
+    tokenized = path.split('/')
+    
     table_name = tokenized[1]
-
-    before_e =  find_last('e',tokenized[2])
-    range_num = int(tokenized[2][before_e+1:])
-
+    range_num = int(tokenized[2][10:])
     page_type = tokenized[3]
     col_num = int(tokenized[4][6:])
 
