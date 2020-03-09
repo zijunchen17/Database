8e6f5f9686798837616192b3999dd9b3598c5d7f
greg graham <greggraham256@gmail.com>
2020-03-08 18:28:23 -0700

    Add lock class

--- lstore/config.py
+++ lstore/config.py
@@ -1,6 +1,6 @@
 # Global Setting for the Database
 # PageSize, StartRID, etc..
-CACHE_SIZE = 250
+CACHE_SIZE = 120 # breaks at <120   test 110 to 250
 RECORD_SIZE = 8
 PAGE_SIZE = 4096
 BASE_PAGES_PER_RANGE = 16
