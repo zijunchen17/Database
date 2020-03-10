4b54bf94fecdcefffb6ccac3329213cb79b83be2
greg graham <greggraham256@gmail.com>
2020-03-10 14:08:52 -0700

    Added lock releases to comm/abort, simpler bound fcn method fetch

--- lstore/transaction.py
+++ lstore/transaction.py
@@ -8,7 +8,8 @@ class Transaction:
     """
     def __init__(self):
         self.queries = []
-        self.writing_queries = []
+        self.write_query_locks = []
+        self.ROLLBACK_METHODS = ["insert", "increment", "update"]
         pass
 
     """
@@ -24,33 +25,33 @@ class Transaction:
     # If you choose to implement this differently this method must still return True if transaction commits or False on abort
     def run(self):
         for query, args in self.queries:
-            print(args)
+            print("args: ",args)
             result = query(*args)
-            
-            print("query obj", query.__self__.table.base_rid)
-            
+
             # If the query has failed the transaction should abort
             if result == False:  # If query couldn't acquire key
                 return self.abort()
-
             # Successfully acquired lock. Note.
             else:
-                q_str = str(query)
-                print("q_Str:", q_str)
-                q_str = q_str.split(' ')
-                q_str = q_str[2]
-                q_str = q_str.split('.')
-                q_str = q_str[-1]
-                if q_str in ["insert", "increment", "update"]: # lol
-                    print("matched")
-                    self.writing_queries.append(query)
+                #TODO: Make cleaner way to do this using .__self__
+                method_name = query.__name__
+                if method_name in self.ROLLBACK_METHODS: # lol
+                    rid = query.__self__.table.key_directory[args[0]]
+                    lock = query.__self__.table.lock_manager[rid]
+                    self.write_query_locks.append(lock)
         return self.commit()
 
     def abort(self):
+    
+        for lock in self.write_query_locks:
+            lock.release_write()
         #TODO: do roll-back and any other necessary operations        
         return False
 
     def commit(self):
+        map(lambda obj: obj.write_query_locks, self.write_query_locks)
+        # same as loop in abort
+
         # TODO: commit to database
         return True
 
