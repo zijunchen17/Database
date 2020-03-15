6ad2f5ca5c72be1744cf526daaa971af96f58581
greg graham <greggraham256@gmail.com>
2020-03-15 13:27:45 -0700

    revert tester

--- transaction_tester.py
+++ transaction_tester.py
@@ -15,13 +15,12 @@ records = {}
 num_threads = 8
 seed(8739878934)
 
-q = Query(grades_table)
 # Generate random records
+q = Query(grades_table)
 for i in range(0, 10000):
     key = 92106429 + i
     keys.append(key)
     records[key] = [key, 0, 0, 0, 0]
-    # q = Query(grades_table) # Moved above for loop
     q.insert(*records[key])
 
 # create TransactionWorkers
@@ -32,21 +31,18 @@ for i in range(num_threads):
 # generates 10k random transactions
 # each transaction will increment the first column of a record 5 times
 q = Query(grades_table)
-for i in range(1000):  # Original amount
-# for i in range(10000): # 8 is 1 each
-    # k = randint(0, 2000 - 1)
-    k = randint(0, 200 - 1) ## test : do this for more transaction conflicts and rollbacks
+for i in range(1000):
+    k = randint(0, 2000 - 1)
     transaction = Transaction()
-    # for j in range(5):
-    for j in range(20): ## test: inside each transcation adding more queries
-        key = keys[k * 5 + j] 
-        # q = Query(grades_table)  # Moved above for loop and consolidated
+    for j in range(5):
+        key = keys[k * 5 + j]
+        # q = Query(grades_table)
         transaction.add_query(q.select, key, 0, [1, 1, 1, 1, 1])
-        # q = Query(grades_table)  # Moved above for loop and consolidated
+        # q = Query(grades_table)
         transaction.add_query(q.increment, key, 1)
     transaction_workers[i % num_threads].add_transaction(transaction)
 
-threads = []    
+threads = []
 for transaction_worker in transaction_workers:
     threads.append(threading.Thread(target = transaction_worker.run, args = ()))
 
@@ -64,10 +60,7 @@ print(num_committed_transactions, 'transaction committed.')
 query = Query(grades_table)
 s = query.sum(keys[0], keys[-1], 1)
 
-# if s != num_committed_transactions * 20:
-if s != num_committed_transactions * 20:
-    # print('Expected sum:', num_committed_transactions * 20, ', actual:', s, '. Failed.')
-    print('Expected sum:', num_committed_transactions * 20, ', actual:', s, '. Failed.')
-    
+if s != num_committed_transactions * 5:
+    print('Expected sum:', num_committed_transactions * 5, ', actual:', s, '. Failed.')
 else:
     print('Pass.')
